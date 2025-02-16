d3a908c926863f4ccb8006f12e9f4a98e7d2c1c8
rijalalfariz <rijalfariz17@gmail.com>
2025-02-16 13:35:46 +0700

    tes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/alfariz-me",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
