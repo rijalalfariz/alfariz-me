47d6b46297c385774d372a733070eef0cef5b6b0
rijalalfariz <rijalfariz17@gmail.com>
2025-02-23 15:24:17 +0700

    imaging2

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   basePath: "/alfariz-me",
   output: "export",
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'picsum.photos',
+            port: '',
+            pathname: '/alfariz-me/**',
+        },
+    ],
+},
 };
 
 // export default nextConfig;
