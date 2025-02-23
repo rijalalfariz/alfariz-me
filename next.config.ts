da8f2e09d1dbdb7f26e02a6f99ee56a4bc33348c
rijalalfariz <rijalfariz17@gmail.com>
2025-02-23 14:28:38 +0700

    build 17

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   basePath: "/alfariz-me",
   output: "export",
   reactStrictMode: true,
-  images: {
-    unoptimized: true
-  },
   distDir: 'out',
   trailingSlash: true
 };
