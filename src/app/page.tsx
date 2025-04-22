ccd4a9f4af4fe22bd33666e0736d0f3a77a97bbd
rijalalfariz <rijalfariz17@gmail.com>
2025-04-22 15:03:08 +0700

    bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-import { Mail, ChevronDown } from 'lucide-react';
+import { Mail } from 'lucide-react';
 import Image from 'next/image';
 
 const PortfolioWebsite = () => {
