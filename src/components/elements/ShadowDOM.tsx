ef7b5919ce681df913063af734fd8b4cc71b2c11
rijalalfariz <rijalfariz17@gmail.com>
2025-07-20 21:32:45 +0700

    6

--- src/components/elements/ShadowDOM.tsx
+++ src/components/elements/ShadowDOM.tsx
@@ -17,15 +17,34 @@ const ShadowDOM: React.FC<ShadowDOMProps> = ({ children, className, style }) => 
     const container = containerRef.current;
     if (!container) return;
 
-    if (!container.shadowRoot) {
-      shadowRootRef.current = container.attachShadow({ mode: 'open' });
+    // if (!container.shadowRoot) {
+    //   shadowRootRef.current = container.attachShadow({ mode: 'open' });
 
+    //   const mountPoint = document.createElement('div');
+    //   shadowRootRef.current.appendChild(mountPoint);
+
+    //   reactRootRef.current = createRoot(mountPoint);
+    //   mountPointRef.current = mountPoint;
+    // } else {
+    //   shadowRootRef.current = container.shadowRoot;
+    //   // Optional: reattach render if necessary
+    // }
+
+    // Create shadow root if it doesn't exist
+    if (!shadowRootRef.current) {
+      if (!container.shadowRoot) {
+        shadowRootRef.current = container.attachShadow({ mode: 'open' });
+      } else {
+        shadowRootRef.current = container.shadowRoot;
+      }
+
+      // Create mount point
       const mountPoint = document.createElement('div');
+      // mountPointRef.current = mountPoint;
       shadowRootRef.current.appendChild(mountPoint);
 
+      // Create root
       reactRootRef.current = createRoot(mountPoint);
-    } else {
-      shadowRootRef.current = container.shadowRoot;
     }
 
     // Render children
